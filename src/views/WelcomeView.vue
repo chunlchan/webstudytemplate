9abe5c2443d0cd066693783432f8a46c8bf4f7ab
chunlchan <c-chan5@northwestern.edu>
2023-12-08 14:09:53 -0600

    disable next button if pid is missing

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -4,7 +4,7 @@
     <p>Instructions here</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eligendi enim, vero ipsum cum voluptatem, ex
       molestiae nesciunt distinctio fugiat beatae voluptatum dicta, et quaerat. Libero beatae eius sint ullam?</p>
-    <v-btn color="primary mt-4" @click="next">Next <v-icon>chevron_right</v-icon></v-btn>
+    <v-btn color="primary mt-4" @click="next" :disabled="store.pid==null || store.pid==''">Next <v-icon>chevron_right</v-icon></v-btn>
   </v-container>
 </template>
 
@@ -12,14 +12,19 @@
 import { onMounted } from 'vue';
 import {useRouter, useRoute } from 'vue-router'
 import { useStore } from "@/stores/store.js"
+import Swal from "sweetalert2";
 const router = useRouter();
 const route = useRoute();
 const store = useStore();
 
 onMounted(() => {
   if (route.query.pid == null || route.query.pid == "") {
-    //show error message
-    alert("missing participant ID")
+    store.pid = null;
+    Swal.fire({
+    title: 'Missing participant ID',
+    icon: 'info',
+    text: 'Try reloading the page or contact the experimenter if this error persists.'
+  })
     return;
   } else {
     store.pid = route.query.pid
