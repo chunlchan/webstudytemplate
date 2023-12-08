1aa1d279445aa333fbd11808da7b85136f2f0755
chunlchan <c-chan5@northwestern.edu>
2023-12-08 15:06:07 -0600

    Loading spinner on route load

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <v-app>
-  <RouterView />
+    <div class="d-flex justify-center mt-5" v-if="routerLoadingStore.routerLoading == true">
+      <v-progress-circular indeterminate></v-progress-circular>
+    </div>
+    <RouterView v-else />
   </v-app>
 </template>
+
+<script setup>
+import { useRouterLoadingStore } from "@/stores/routerLoadingStore.js"
+const routerLoadingStore = useRouterLoadingStore();
+</script>
